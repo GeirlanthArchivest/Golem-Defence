7acc030e3c308a5031c1c71cca38cf74b94e6f51
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2024-04-16 10:53:14 +0100

    added invisible projectile for punch

--- Golem Defence/Assets/Scripts/PlayerMovement.cs
+++ Golem Defence/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int runSpeed = 1;
+    public int bulletSpeed;
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
 
     float horizontal;
     float vertical;
+
+    private float nextFireTime = 0f;
+    public float fireRate = 0.5f;
+
 
 
     // Start is called before the first frame update
@@ -22,11 +29,26 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (Time.time >= nextFireTime)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                shootBullet();
+                nextFireTime = Time.time + fireRate;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(horizontal * runSpeed, vertical * runSpeed, 0.0f);
         transform.position = transform.position + movement * Time.deltaTime;
+    }
+
+    private void shootBullet()
+    {
+        var bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = shootingPoint.right * bulletSpeed;
     }
 }
