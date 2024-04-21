d8c22851d4efe8c5f2b0ff101bed59df264e6043
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2024-04-21 21:06:49 +0100

    added basic enemy movement

--- Golem Defence/Assets/Scripts/Enemy.cs
+++ Golem Defence/Assets/Scripts/Enemy.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float currentHealth;
+    public int currentHealth;
     public int maxhealth = 5;
+    public float speed;
+    public float chaseDistance;
+    public float stopDistance;
+    public GameObject target;
+
+    private float targetDistance;
 
     private void Start()
     {
         currentHealth = maxhealth;
     }
 
+    void Update()
+    {
+        targetDistance = Vector2.Distance(transform.position, target.transform.position);
+        if (targetDistance < chaseDistance && targetDistance > stopDistance)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasePlayer();
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D collisioninfo)
     {
@@ -19,6 +37,20 @@ public class Enemy : MonoBehaviour
         {
             TakeDamage(PlayerMovement.damage);
         }
+    }
+
+    private void StopChasePlayer()
+    {
+
+    }
+
+    private void ChasePlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        /*if (transform.position.x < target.transform.position.x)
+        {
+
+        }*/
     }
 
     public void TakeDamage(int damage)
