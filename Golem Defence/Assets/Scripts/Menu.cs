0ff75c62048a47f4707f52ef613a1c26fa762038
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2024-05-14 09:35:07 +0100

    added instructions page

--- Golem Defence/Assets/Scripts/Menu.cs
+++ Golem Defence/Assets/Scripts/Menu.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject instructions;
+    //[SerializeField] GameObject levelSelect;
+    [SerializeField] GameObject backButton;
+    [SerializeField] GameObject playButton;
+
     public void PlayGame(string targetSceneName)
     {
         SceneManager.LoadScene(targetSceneName);
+    }
+
+    public void Instructions()
+    {
+        mainMenu.SetActive(false);
+        instructions.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(backButton.gameObject);
+    }
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        instructions.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(playButton.gameObject);
     }
 
     public void QuitGame()
