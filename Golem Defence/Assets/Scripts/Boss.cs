890a1c776fc1baa53217977e9c57688f87c7f62a
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2024-05-28 10:30:17 +0100

    added a final player 1 & 2 onto the healthbars so you know which is whose

--- Golem Defence/Assets/Scripts/Boss.cs
+++ Golem Defence/Assets/Scripts/Boss.cs
@@ -8,7 +8,7 @@ public class Boss : MonoBehaviour
     public int currentHealth;
     public int bulletSpeed;
     public int maxhealth = 25;
-    public static int damage = 25;
+    public static int damage = 20;
     public float speed;
     public float chaseDistance;
     public float stopDistance;
